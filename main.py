75978e0aec61483b29d829a9751ceeb9869687e0
Joan Boulangger <59853046+BouCode@users.noreply.github.com>
2022-01-18 11:26:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 
 def mininet ():
-    print ('Bienvenidos al instalador de mininet')
     os.system('sudo apt update')
-    print ('Instalando git ...')
     os.system('sudo apt install git -y')
     os.system('git clone git://github.com/mininet/mininet')
     os.system('cd mininet && git checkout -b mininet-2.3.0 2.3.0')
     os.system('cd mininet/util && ./install.sh')
     os.system('sudo mn --switch ovsbr --test pingall')
 
+def opendaylight (): 
+    print ('Opendaylight')
+    os.system('sudo apt -y install openjdk -8-jre')
+    os.system('sudo update-alternatives --config java')
+    os.system("sudo bash -c \'echo JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64/jre\"\'")
+    os.system("source /etc/environment")
+    os.system('mkdir opendaylight && cd opendaylight && wget https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/karaf/0.8.3/karaf-0.8.3.tar.gz && tar -zxvf karaf-0.8.3.tar.gz')
+    os.system('cd opendaylight/karaf-0.8.3/bin && sudo ./karaf')
+
+
+
 if __name__ == '__main__':
-    mininet()
+    opendaylight()
