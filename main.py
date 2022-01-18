303e776fec1d10368655d0d696d9cb3669ba8f1f
Joan Boulangger <59853046+BouCode@users.noreply.github.com>
2022-01-18 11:09:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import os
 def mininet ():
     print ('Bienvenidos al instalador de mininet')
     os.system('sudo apt update')
+    print ('Instalando git ...')
     os.system('sudo apt install git -y')
-    os.system('git clone git://github.com/mininet/mininet && cd mininet')
-    os.system('cd mininet')
-    os.system('git checkout -b mininet-2.3.0 2.3.0')
-    os.system('cd util')
-    os.system('sudo ./install.sh')
+    os.system('git clone git://github.com/mininet/mininet')
+    os.system('cd mininet && git checkout -b mininet-2.3.0 2.3.0')
+    os.system('cd mininet/util && ./install.sh')
     os.system('sudo mn --switch ovsbr --test pingall')
-
-    
+=
 if __name__ == '__main__':
     mininet()
-
